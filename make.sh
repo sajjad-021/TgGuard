0dd6cc96233b9414ce83b71895290b50c9cc00c4
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-08-21 21:32:43 -0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
@@ -17,7 +17,27 @@ lualibs=(
 'serpent'
 )
 
-logo() {
+today=`date +%F`
+
+get_sub() {
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c; do
+        if $flag; then
+            printf '%c' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]; then
+                count=0
+            else
+                ((count++))
+                if ((count > 1)); then
+                    flag=true
+                fi
+            fi
+        fi
+    done
+}
+
+function logo() {
     declare -A logo
     seconds="0.004"
 logo[0]="  .          '||    ||' '||'''|  '||    ||' '||'|.  '||'''|  '||''|."
@@ -37,26 +57,26 @@ printf "\e[38;5;213m\t"
         done
         printf "\n\t"
     done
-printf "\n"
+printf "\e[0;00m\n"
 }
 
-log() {
-  echo -e "\033[38;5;105m .               /|\,/ |\,  ,- _~,   /\|,/ \|,   - _,,     ,- _~,  -_ /\,\033[0;00m"
-  echo -e "\033[38;5;142m||    _          /| || ||   (' /| /  /| || ||     -/  )   (' /| /   || ,,\033[0;00m"
-  echo -e "\033[38;5;099m=||=  / \\        || || ||  ((  ||/=  || || ||    ~||_<   ((  || =  /|| /|.\033[0;00m"
-  echo -e "\033[38;5;034m||  || ||        ||=|= ||  ((  ||    ||=|= ||     || |\  (( |||    \||/-\,\033[0;00m"
-  echo -e "\033[38;5;406m||  || ||       ~|| || ||   ( / |   ~|| || ||     ,/--||  (   |     ||  \.,\033[0;00m"
-  echo -e "\033[38;5;129m||  \|_-|        |, |\,|\,   -____-  |, |\,|\,   _--_-'    -____- _---_-|.\033[0;00m"
-         echo -e "\033[38;5;129m||   -_-_\033[0;00m"
-            echo -e "\033[38;5;129m,_-_-\.\033[0;00m"
- }
+is() {
+	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y; sudo apt-get -y upgrade; sudo apt-get -y update
+  sudo apt-get install git redis-server libreadline-dev -y libconfig-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y libconfig8-dev libjansson-dev lua5.2 liblua5.2-dev lua-lgi glib-2.0 libnotify-dev libssl-dev libssl1.0.0 make libstdc++6 g++-4.9 unzip autoconf g++ -y libpython-dev -y expat libexpat1-dev -y libreadline-gplv2-dev libreadline5-dev tmux -y
+ sudo apt-get install lua5.1 luarocks lua-socket lua-sec redis-server curl
+}
 
-tg() {
-echo -e "\e[38;5;099minstall telegram-cli\e"
-    rm -rf ../.telegram-cli
-    wget https://valtman.name/files/telegram-cli-1222
-    mv telegram-cli-1222 telegram-cli
-    chmod +x telegram-cli
+get_firstname_by_username() {
+	user=`./.bin/telegram-cli --disable-link-preview -C -R -D -e \
+	'resolve_username '${1}'' 2>/dev/null`
+    space_index=`echo ${user} | tr -cd ' '  | wc -c`
+	msg=null
+	if [[ ! ${user} ]] || [[ ${user} =~ 400 ]]; then
+		echo $msg
+	else
+		msg=`echo ${user} | cut -d ' ' -f${space_index}- | cut -d ' ' -f1`
+		echo "${msg}"
+	fi
 }
 
 make_progress() {
@@ -65,13 +85,6 @@ exe=`lua <<-EOF
 EOF
 `
     echo ${exe:0:4}
-}
-
-install2() {
-echo -e "\e[38;5;034mInstalling more dependencies\e"
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y; sudo apt-get -y upgrade; sudo apt-get -y update
-  sudo apt-get install git redis-server libreadline-dev -y libconfig-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y libconfig8-dev libjansson-dev lua5.2 liblua5.2-dev lua-lgi glib-2.0 libnotify-dev libssl-dev libssl1.0.0 make libstdc++6 g++-4.9 unzip autoconf g++ -y libpython-dev -y expat libexpat1-dev -y libreadline-gplv2-dev libreadline5-dev tmux -y
- sudo apt-get install lua5.1 luarocks lua-socket lua-sec redis-server curl
 }
 
 function download_libs_lua() {
@@ -101,6 +114,12 @@ function configure() {
         make bootstrap &>/dev/null
     fi
     cd ..; rm -rf luarocks*
+    if [[ ${1} != "--no-download" ]]; then
+        download_libs_lua
+        wget --progress=bar:force https://valtman.name/files/telegram-cli-1214
+        if [ ! -d ".bin" ]; then mkdir .bin; fi
+        mv telegram-cli-1214 ./.bin/telegram-cli; chmod +x ./.bin/telegram-cli
+    fi
     for ((i=0;i<101;i++)); do
         printf "\rConfiguring... [%i%%]" $i
         sleep 0.007
@@ -108,41 +127,51 @@ function configure() {
     printf "\nDone\n"
 }
 
-install() {
-echo -e "\e[38;5;035mUpdating packages\e"
-   sudo apt-get update -y
-   sudo apt-get upgrade -y
-
-echo -e "\\e[38;5;129mInstalling dependencies\e"
-install2
-configure
-sudo apt -u update && sudo apt -y upgrade && sudo apt -y autoclean && sudo apt -y 
-echo -e "\e[38;5;046mInstalling packages successfully\033[0;00m"
+conf() {
+	mkdir -p "$THIS_DIR"/.bin/TG"$1"
+	cfg="$THIS_DIR"/.bin/TG$1/TG.cfg
+	TG="$THIS_DIR"/.bin/TG-$1.lua
+	if [[ ! -f $cfg ]]; then
+		echo "default_profile = \"TG$1\";TG$1 = {config_directory = \"$THIS_DIR/.bin/TG$1\";test = false;msg_num = true;};" >> $cfg
+	fi
+	if [[ ! -f $TG ]]; then
+		cat main.lua >> TG-$1.lua
+		sed -i 's/ADS-ID/'$1'/g' TG-$1.lua
+		mv TG-$1.lua ./.bin
+	fi
 }
 
-if [ "$1" = "install" ]; then
-install
-fi
+install() {
+logo
+is
+configure
+sudo apt -u update && sudo apt -y upgrade && sudo apt -y autoclean && sudo apt -y autoremove
+clear
+logo
+ echo -e "\033[38;5;034mSuccessful install\033[0;00m\n\n\n"
+ echo -e "Now, you can run your bot by => \033[38;5;123m./TG 1~9\033[0;00m <= command"
+ echo -e "for example: ./TG 1\n\n"
+ echo -e "you can use anticrash by => \033[38;5;142mscreen -S nohup ./TG 1~9\033[0;00m <= command"
+ echo -e "for example: screen -S nohup ./TG 7\n\n"
+}
 
-if [ "$1" = "api" ]; then
-while true; do
-screen -S nohup lua api.lua
-done
-fi
-
-if [ "$1" = "bot" ]; then
-while true; do
-screen -S nohup ./telegram-cli -s tgGuard.lua
-done
-fi
-
-if [ ! -f telegram-cli ]; then
-    echo -e "\033[38;5;208mError! telegram-cli not found, Please reply to this message:\033[1;208m"
+	if [[ "$1" =~ ^[0-9]+$ ]] ; then
+	conf $1
+COUNTER=0
+  while [ $COUNTER -lt 5 ]; do
+       tmux kill-session -t TG$1
+	        tmux new-session -s TG$1 "./.bin/telegram-cli -s ./.bin/TG-$1.lua -E -c ./.bin/TG$1/TG.cfg "$@""
+        tmux detach -s TG$1
+    sleep 1
+  done
+	fi
+	
+if [ ! -f "./.bin/telegram-cli" ]; then
+    echo -e "\033[38;5;208mError! telegram-cli not found, Please reply to this message:\033[0;00m"
     read -p "Do you want to install and config? [y/n] = "
 	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-	chmod 777 make.sh
-        install
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+					install
+     elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
         exit 1
   fi
 fi
