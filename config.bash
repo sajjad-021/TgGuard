d9d4ee3d284aa7f173f8bb206febd09bdee7da6a
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-09-20 11:52:56 -0700

    Update config.bash

--- config.bash
+++ config.bash
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-THIS_DIR=$(cd $(dirname $0); pwd)
-cd $THIS_DIR
+THIS_DIR=$(cd "$(dirname "$0")"; pwd)
+cd "$THIS_DIR"
 
-logo() {
+function logo() {
     declare -A logo
-    seconds="0.004"
+    seconds="0.002"
 logo[0]="  .          '||    ||' '||'''|  '||    ||' '||'|.  '||'''|  '||''|."
 logo[1]=".||.   ... .  |||  |||   || .     |||  |||   ||  ||  || .     ||   ||"
 logo[2]=" ||   || ||   |'|..'||   ||'|     |'|..'||   ||''|.  ||'|     ||''|'"
 logo[3]=" ||    |''    | '|' ||   ||       | '|' ||   ||   || ||       ||   |."
 logo[4]=" '|.' '|||.  .|. | .||. .||....| .|. | .||. .||..|' .||....| .||.  '|'"
 logo[5]="    .|...'"
-logo[6]="➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖"
+logo[6]=""
 logo[7]="Channel : @tgMember"
 logo[8]="Develop by @sajjad_021"
 printf "\e[38;5;213m\t"
@@ -26,15 +26,29 @@ printf "\e[38;5;213m\t"
 printf "\n"
 }
 
-tgcli_version=1222
-luarocks_version=2.4.2
+function update() {
+  sudo git pull
+  sudo git fetch --all
+  sudo git reset --hard origin/master
+  sudo git pull origin master
+  sudo chmod +x TG
+}
+
+tgcli_version=1214
 
 lualibs=(
 'luasec'
+'socket'
 'luarepl'
 'lbase64 20120807-3'
 'luafilesystem'
 'lub'
+'auth'
+'lua-term'
+'Lua-cURL'
+'multipart-post'
+'lanes'
+'multipart-post'
 'luaexpat'
 'redis-lua'
 'lua-cjson'
@@ -45,33 +59,35 @@ lualibs=(
 'serpent'
 )
 
+pkg=(
+'git'
+'wget'
+'openssl'
+'coreutils'
+'make'
+'gcc'
+'libreadline-dev'
+'libssl-dev'
+'redis-server'
+'libssl-dev'
+'unzip'
+'libexpat1-dev'
+'libconfig-dev'
+'tmux'
+'lua5.2'
+'liblua5.2-dev'
+'lua-socket'
+'lua-sec'
+'lua-expat'
+'libevent-dev'
+'autoconf'
+'g++'
+'libjansson-dev'
+'expat'
+'luarocksv'
+)
+
 today=`date +%F`
-
-get_sub() {
-    local flag=false c count cr=$'\r' nl=$'\n'
-    while IFS='' read -d '' -rn 1 c; do
-        if $flag; then
-            printf '%c' "$c"
-        else
-            if [[ $c != $cr && $c != $nl ]]; then
-                count=0
-            else
-                ((count++))
-                if ((count > 1)); then
-                    flag=true
-                fi
-            fi
-        fi
-    done
-}
-
-make_progress() {
-exe=`lua <<-EOF
-    print(tonumber($1)/tonumber($2)*100)
-EOF
-`
-    echo ${exe:0:4}
-}
 
 function download_libs_lua() {
     if [[ ! -d "logs" ]]; then mkdir logs; fi
@@ -79,47 +95,33 @@ function download_libs_lua() {
     local i
     for ((i=0;i<${#lualibs[@]};i++)); do
         printf "\r\33[2K"
-        printf "\rtgAds: wait... [`make_progress $(($i+1)) ${#lualibs[@]}`%%] [$(($i+1))/${#lualibs[@]}] ${lualibs[$i]}"
-        ./.luarocks/bin/luarocks install ${lualibs[$i]} &>> logs/logluarocks_${today}.txt
+        printf "\rtgAds: Please Wait ... [$(($i+1))/${#lualibs[@]}] ${lualibs[$i]}"
+        "$HOME"/.luarocks/bin/luarocks install ${lualibs[$i]} &>> logs/logluarocks_${today}.txt
     done
     sleep 0.2
     printf "\nLogfile created: $PWD/logs/logluarocks_${today}.txt\nDone\n"
-    rm -rf luarocks-2.2.2*
-}
-
-function update() {
-  git pull
-  git fetch --all
-  git reset --hard origin/master
-  git pull origin master
-  chmod +x TG
 }
 
 function configure() {
     if [[ -f "/usr/bin/lua5.3" ]] || [[ -f "/usr/bin/lua5.1" ]] || [[ -f "/usr/local/bin/lua5.3" ]]; then
-    	sudo apt remove -y lua5.3
-	sudo apt -y autoremove
-	sudo apt install -y lua5.2
-        echo -e "\033[0;31mError\033[0m":\
-        "tgAds ins't working with lua5.3 and others versions, the packages must be removed,"\
-        "please remove them, reinstall lua5.2 and run \033[0;31mbash TG\033[0m again."
+    	sudo apt remove -y lua5.3 &>/dev/null
+	    sudo apt -y autoremove &>/dev/null
+	    sudo apt install -y lua5.2 &>/dev/null
+      echo -e "\n\033[0;31m TeleGram Advertising Download Libs ... \033[0m\n"
      fi
-    dir=$PWD
-    wget http://luarocks.org/releases/luarocks-${luarocks_version}.tar.gz &>/dev/null
-    tar zxpf luarocks-${luarocks_version}.tar.gz &>/dev/null
-    cd luarocks-${luarocks_version}
-    if [[ ${1} == "--no-null" ]]; then
-        ./configure --prefix=$dir/.luarocks --sysconfdir=$dir/.luarocks/luarocks --force-config
-        make bootstrap
-    else
-        ./configure --prefix=$dir/.luarocks --sysconfdir=$dir/.luarocks/luarocks --force-config &>/dev/null
-        make bootstrap &>/dev/null
-    fi
-    cd ..; rm -rf luarocks*
+  	git clone https://github.com/keplerproject/luarocks.git &>/dev/null
+  	cd luarocks
+	
+  	PREFIX="$HOME/.luarocks"
+
+  	./configure --prefix="$PREFIX" --sysconfdir="$PREFIX"/luarocks --force-config &>/dev/null
+  	make build &>/dev/null
+	sudo make install &>/dev/null
+	make bootstrap &>/dev/null
+		cd ..
+		rm -rf luarocks
     if [[ ${1} != "--no-download" ]]; then
         download_libs_lua
-        wget --progress=bar:force https://valtman.name/files/telegram-cli-${tgcli_version} 2>&1 | get_sub
-        mv telegram-cli-${tgcli_version} telegram-cli; chmod +x telegram-cli
     fi
     for ((i=0;i<101;i++)); do
         printf "\rConfiguring... [%i%%]" $i
@@ -128,51 +130,84 @@ function configure() {
     printf "\nDone\n"
 }
 
-install() {
-    sudo chmod 777 TG
-sudo apt update -y && sudo apt upgrade -y
-  sudo apt-get install -f
-	sudo dpkg -a --configure
-	sudo apt-get dist-upgrade
-	sudo sudo apt-get dist-upgrade
- echo -e "\e\n[38;5;035mUpdating packages\e\n\n"
-		     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  sudo apt-get install c++ -y libreadline-dev -y libstdc++6 -y libnotify-dev -y libconfig-dev -y lua-lgi -y luarocks -y libssl-dev -y lua5.2 -y liblua5.2-dev -y libevent-dev -y libjansson-dev -y libpython-dev -y lua-socket -y lua-sec -y lua-expat -y make unzip git redis-server autoconf g++ -y expat libexpat1-dev -y
-  echo -e "\\e[38;5;129mInstalling dependencies\e"
-rm -rf README.md
-    configure ${2}    
-   rm -rf logs
-  sudo apt -y autoremove
-  sudo service redis-server restart
-  sudo service redis-server start
+function installation() {
+for i in $(seq 1 100); do  
+    sleep 0.1
+    sudo apt-get -y install $pkg &>/dev/null
+    if [ $i -eq 100 ]; then
+        echo -e "XXX\n100\nDone!\nXXX"
+    elif [ $(($i % 4)) -eq 0 ]; then
+        let "is = $i / 4"
+        echo -e "XXX\n$i\n${pkg[is]}\nXXX"
+    else
+        echo $i
+    fi 
+done | whiptail --title 'TeleGram Guard Robot Install and Configuration' --gauge "${pkg[0]}" 6 60 0
+}
+
+tgcli_config() {
+tgdr="$THIS_DIR"\bin
+if [ ! -d $tgdr ]; then
+  mkdir -p "$THIS_DIR"/bin
+  printf '%s\n' "
+default_profile = \"tgGuard\";
+tgGuard = {
+  config_directory = \"$THIS_DIR/bin\";
+  auth_file = \"$THIS_DIR/bin\auth\";
+  test = false;
+  msg_num = true;
+  log_level = 2;
+};
+" > "$THIS_DIR"/bin/config.cfg
+fi
+}
+
+conf() {
+AP="$THIS_DIR"/start
+if [ ! -f $AP ]; then
+ read -p "\n\033[1;33mDo you want api config?  [y/n]\n\033[0;00m\n\033[6;48m\n"
+	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+	 echo -e "\n\033[38;5;27mPut your Token\n\033[38;5;208m\n\033[6;48m\n"
+read -rp '' TKN
+ echo "#!/bin/bash
+	while true; do
+       		sudo tmux kill-session -t tgGuard
+			sudo tmux new-session -s tgGuard './telegram-cli --disable-link-preview -R -C -v -s tgGuard.lua -I -l 1 -E -c config.cfg -p tgGuard --bot=$TKN -L Log-api.txt &>/dev/null'
+        	sudo tmux detach -s tgGuard
+	done" >> start
+	chmod +x start
+    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+    echo "#!/bin/bash
+     while true; do
+       sudo tmux kill-session -t tgGuard
+		sudo tmux new-session -s tgGuard ./telegram-cli --disable-link-preview -R -C -v -s tgGuard.lua -I -l 1 -E -c config.cfg -p tgGuard -L log.txt &>/dev/null'
+        sudo tmux detach -s tgGuard
+	done" >> start
+	chmod +x start
+fi
 }
 
 if [ "$1" = "upgrade" ]; then
 update
 fi
 
-conf() {
-AP="$THIS_DIR"/start.sh
-if [ ! -f $AP ]; then
-    echo "#!/bin/bash
-     while true; do
-       sudo tmux kill-session -t tgGuard
-		sudo tmux new-session -s tgGuard "./telegram-cli --disable-link-preview -R -C -v -s tgGuard.lua -I -l 1 -E -p tgGuard -L log.txt"
-        sudo tmux detach -s tgGuard
-	done" >> start.sh
-	chmod 777 start.sh
-fi
-}
-
 start() {
 while true; do
-	sudo screen -S nohup ./start.sh
+	screen -S nohup ./start
 done
 }
 
 if [ ! -f "telegram-cli" ]; then
-    install
-    conf
+  	chmod 777 config.bash
+	logo
+	installation
+	rm -rf README.md
+ 	configure
+	wget "https://valtman.name/files/telegram-cli-1222" &>/dev/null
+	rm -rd logs
+	mv telegram-cli-1222 telegram-cli; chmod +x telegram-cli
+	tgcli_config
+ 	conf
 else
-   start
+	start
 fi
